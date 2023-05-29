c4e5a91f8c121c7dd944e188161e7cfb4a8e059b
gaetan <Nuxnuxx>
2023-05-29 07:09:00 +0200

    server: setup database and prisma + test creation and find in index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,45 @@
-const saucisse = 'saucisse'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+
+  const user = await prisma.user.create({
+    data: {
+      email: 'nuxnux02290@gmail.com',
+      username: 'nuxnux',
+      password: 'saucisse',
+    }
+  })
+
+  console.log(user)
+
+  const userFound = await prisma.user.findFirst({
+    where: {
+      username: 'nuxnux'
+    }
+  })
+
+  console.log(userFound)
+}
+
+
+main()
+
+  .then(async () => {
+
+    await prisma.$disconnect()
+
+  })
+
+  .catch(async (e) => {
+
+    console.error(e)
+
+    await prisma.$disconnect()
+
+    process.exit(1)
+
+  })
--- server/todo.md
+++ server/todo.md
@@ -4,7 +4,7 @@
     - Setup typscript DONE
     - Setup prettier DONE
     - Setup eslint DONE
-    - Setup database and prisma
+    - Setup database and prisma DONE
     - Setup config for dev | test | prod
 
 ## Server
