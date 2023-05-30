6075182dc131ced1914f3fa27873f3dad6655342
ES0TERIX <thomas.carre74@gmail.com>
2023-05-30 18:11:04 +0700

    client: addedthe scss files

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './assets/sass/_index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />,
