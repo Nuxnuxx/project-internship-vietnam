f681cea271605a3a8233ec8aead28bbe0d14d0d2
gaetan <Nuxnuxx>
2023-05-30 07:00:39 +0200

    fix: cors not enabled properly

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 import userRouter from './router/userRouter'
 import apiRouter from './router/apiRouter'
 
@@ -7,6 +8,7 @@ import apiRouter from './router/apiRouter'
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
