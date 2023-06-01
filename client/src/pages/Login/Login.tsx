eb220f4e57bf781c476fe5fde7ee993be66b49ba
ES0TERIX <thomas.carre74@gmail.com>
2023-06-01 12:26:16 +0700

    client: login and register adjustment, useMutation on Register

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -4,45 +4,48 @@ import { useQuery } from '@tanstack/react-query'
 import fetchLogin from './fetchLogin'
 import { useAppSelector } from '../../utils/hooks'
 import Header from '../../Header'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-    const formLoginData = useAppSelector((state) => state.formLoginData)
-    const dispatch = useDispatch()
-    const results = useQuery(['form', formLoginData], fetchLogin)
-    const token = results?.data?.token ?? ''
+  const formLoginData = useAppSelector((state) => state.formLoginData)
+  const dispatch = useDispatch()
+  const results = useQuery(['form', formLoginData], fetchLogin)
+  const token = results?.data?.token ?? ''
 
-    return (
-        <>
-        <Header/>
-        <div className='login-form'>
-            <form
-                onSubmit={(e) => {
-                    e.preventDefault()
-                    const formData = new FormData(e.currentTarget)
-                    const obj = {
-                        email: formData.get('email') ?? '',
-                        password: formData.get('password') ?? '',
-                    }
-                    dispatch(all(obj))
-                }}
-            >
-                <label htmlFor='email'>
-                    email
-                    <input id='email' name='email' placeholder='E-Mail' />
-                </label>
+  return (
+    < >
+      <Header/>
+      <div className='container-center-collumn'>
+        <h1>Login</h1>
+        <form className='user-form'
+          onSubmit={(e) => {
+            e.preventDefault()
+            const formData = new FormData(e.currentTarget)
+            const obj = {
+              email: formData.get('email') ?? '',
+              password: formData.get('password') ?? '',
+            }
+            dispatch(all(obj))
+          }}
+        >
+          <label htmlFor='email'>
+            <h3>email</h3> 
+            <input id='email' name='email' placeholder='E-Mail' />
+          </label>
 
-                <label htmlFor='password'>
-                    Password
-                    <input id='password' name='password' placeholder='Password' />
-                </label>
+          <label htmlFor='password'>
+            <h3>Password</h3>
+            <input id='password' name='password' placeholder='Password' type="password" />
+          </label>
 
-                <button>Submit</button>
+          <button>Login</button>
 
-                <h2>{token}</h2>
-            </form>
-        </div>
-        </>
-    )
-}
+          <h2>{token}</h2>
+        </form>
+        <p>Not Registered ?</p>
+        <Link to="/register">Click Here</Link>
+      </div>
+    </> 
+  )}
 
 export default Login
--- client/src/pages/Register/fetchRegister.ts
+++ client/src/pages/Register/fetchRegister.ts
@@ -1,39 +1,39 @@
-import { QueryFunction } from "@tanstack/react-query";
+import { MutationFunction} from "@tanstack/react-query";
 
 type RegisterFormParams = {
-    value: {
-        email: string
-        username: string
-        password: string
-    }
+  value: {
+    email: string
+    username: string
+    password: string
+  }
 }
 
 type APIRegisterResponse = {
-    token: string
+  token: string
 }
 
-const fetchRegister: QueryFunction<APIRegisterResponse, ['form', RegisterFormParams]> = async ({
-    queryKey,
-}) => {
-    const { email, username, password } = queryKey[1].value
+const fetchRegister:MutationFunction<APIRegisterResponse, ['form', RegisterFormParams]> = async (
+  mutationKey 
+) => {
+  const { email, username, password } = mutationKey[1].value
 
-    if (!email || !username || !password) {
-        throw new Error('error in form')
-    }
+  if (!email || !username || !password) {
+    throw new Error('error in form')
+  }
 
-    const token = await fetch('http://localhost:3001/user/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email,
-            username,
-            password,
-        })
+  const token = await fetch('http://localhost:3001/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      username,
+      password,
     })
-    if (!token.ok) throw new Error(`no token for: ${email}, ${username}, ${password}`)
+  })
+  if (!token.ok) throw new Error(`no token for: ${email}, ${username}, ${password}`)
 
-    return token.json()
+  return token.json()
 }
 export default fetchRegister
