f8510492d1bd709f12e3a4fd1b9b8970dcc77bd8
ES0TERIX <thomas.carre74@gmail.com>
2023-06-07 16:08:24 +0700

    client: change: indentation

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -2,20 +2,16 @@
 //
 
 type Product = {
-  id: string;
-  name: string;
-  description: string;
-  price: number;
-  imageUrl: string;
-  category: CATEGORY;
-  quantityInStock: number;
-  createdAt: string;
-  CartItem: CartItem[];
-  OrderItem: OrderItem[];
-};
+  id: string
+  name: string
+  description: string
+  price: number
+  imageUrl: string
+  category: CATEGORY
+  quantityInStock: number
+  createdAt: string
+  CartItem: CartItem[]
+  OrderItem: OrderItem[]
+}
 
-type CATEGORY =
-  | 'CATEGORY1'
-  | 'CATEGORY2'
-  | 'CATEGORY3'
-  | 'CATEGORY4';
+type CATEGORY = 'CATEGORY1' | 'CATEGORY2' | 'CATEGORY3' | 'CATEGORY4'
