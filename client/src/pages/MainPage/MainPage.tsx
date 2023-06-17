d76753331ce863576f04c72f3393158337430e3a
ES0TERIX <thomas.carre74@gmail.com>
2023-06-17 10:24:14 +0700

    client: add chat bot on the main page, add files to components.scss, add color to the color schem

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import Header from '../../Header'
+import ChatBot from '../../components/ChatBot/ChatBot.tsx'
 
 const MainPage = () => {
   return (
     <>
       <Header />
+      <ChatBot />
       Main page
     </>
   )
