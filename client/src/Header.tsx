2f028259ca8d5838d6e8425eaa57b0a1469eff5d
ES0TERIX <thomas.carre74@gmail.com>
2023-06-17 10:16:50 +0700

    client: clicable icon om header

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -10,23 +10,30 @@ const Header = () => {
     <div className='header'>
       {!token ? (
         <div className='user-section'>
-          <img src='../src/assets/img/user-icon.svg' />
-          <Link to='/login'>Login</Link>
+          <Link to='/login' className='user-section'>
+            Login
+            <img src='../src/assets/img/user-icon.svg' />
+          </Link>
         </div>
       ) : (
-        <div className='user-section'>
+        <div className='user-section'
+          onClick={() => dispatch(set(undefined))}>
+          Log Out
           <img src='../src/assets/img/user-icon.svg' />
-          <a onClick={() => dispatch(set(undefined))}>Log Out</a>
         </div>
       )}
 
       <div className='user-section'>
-        <img src='../src/assets/img/home-icon.svg' />
-        <Link to='/catalog'> Catalog </Link>
+        <Link to='/catalog' className='user-section'>
+          <img src='../src/assets/img/home-icon.svg' />
+          Catalog 
+        </Link>
       </div>
       <div className='cart-section'>
-        <img src='../src/assets/img/cart-icon.svg' />
-        <Link to='/cart'> Cart </Link>
+        <Link to='/cart' className='cart-section'> 
+          <img src='../src/assets/img/cart-icon.svg' />
+          Cart 
+        </Link>
       </div>
     </div>
   )
